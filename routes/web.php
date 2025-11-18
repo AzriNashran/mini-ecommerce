e847b44b0136f0e907ce2dbb474ef5df5ef42a19
azri <aeran.nash11@gmail.com>
2025-11-18 18:43:42 +0800

    redirect to /login

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return view('welcome');
+  if (auth()->check()) {
+    return redirect()->route('dashboard');
+  }
+  return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
